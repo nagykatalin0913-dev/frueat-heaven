79b4f23051a4efcdef4ec4b228487d2d6d47df0f
nagykatalin0913-dev <nagykatalin0913@gmail.com>
2026-05-07 14:53:34 +0200

    Add files via upload

---
+++ service-worker.js
@@ -0,0 +1,46 @@
+// Fru'eat Heaven – Service Worker v3.0
+// CSAK PWA cache – OneSignal külön van a /push/ mappában!
+
+const CACHE_NAME = 'frueat-v3';
+const ASSETS = [
+  './',
+  './index.html',
+  './manifest.json',
+  './icon-192.png',
+  './icon-512.png',
+  './bg.png'
+];
+
+self.addEventListener('install', event => {
+  event.waitUntil(
+    caches.open(CACHE_NAME)
+      .then(cache => cache.addAll(ASSETS))
+      .then(() => self.skipWaiting())
+  );
+});
+
+self.addEventListener('activate', event => {
+  event.waitUntil(
+    caches.keys().then(keys =>
+      Promise.all(keys.filter(k => k !== CACHE_NAME).map(k => caches.delete(k)))
+    ).then(() => self.clients.claim())
+  );
+});
+
+self.addEventListener('fetch', event => {
+  if (event.request.url.includes('script.google.com') ||
+      event.request.url.includes('fonts.googleapis.com') ||
+      event.request.url.includes('api.zippopotam.us') ||
+      event.request.url.includes('onesignal.com')) {
+    return;
+  }
+  event.respondWith(
+    fetch(event.request)
+      .then(response => {
+        const clone = response.clone();
+        caches.open(CACHE_NAME).then(cache => cache.put(event.request, clone));
+        return response;
+      })
+      .catch(() => caches.match(event.request))
+  );
+});
